e2d1aea7caea37a4ce853afb0c8ad3c5360c4cda
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-05-15 20:24:57 -0500

    user

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -51,7 +51,50 @@ router.post("/register", async (req, res) => {
             res.status(500).json({
                 error: `Failed to register user: ${err}`
             })
+        };
+    }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+    let {email, password} = req.body.user;
+
+    try {
+        const loginUser = await UserModel.findOne({
+            where: {
+                email
+            }
+        });
+
+        if(loginUser){
+            const passwordComparison = await bcrypt.compare(password, loginUser.password);
+
+            if(passwordComparison){
+                const token = jwt.sign(
+                    {id: loginUser.id},
+                    process.env.JWT_SECRET,
+                    {expiresIn: 60 * 60 * 24}
+                );
+
+                res.status(200).json({
+                    message: "Successful login",
+                    user: loginUser,
+                    token
+                })
+            } else {
+                res.status(401).json({
+                    message: "Incorrect email or password"
+                })
+            }
+        } else {
+            res.status(401).json({
+                message: "Incorrect email or password"
+            })
         }
+    } catch(err){
+        res.status(500).json({
+            message: "Error logging in"
+        })
     }
 })
 
